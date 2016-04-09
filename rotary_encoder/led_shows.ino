4ce5b1195290501b9c7bcac9238e4e400a262563
Benjamin Bergman <ben@benbergman.ca>
2016-04-09 15:49:16 -0500

    Create led_shows.ino

---
+++ rotary_encoder/led_shows.ino
@@ -0,0 +1,202 @@
+#include <ClickEncoder.h>
+#include <TimerOne.h>
+
+#include <Adafruit_NeoPixel.h>
+
+#define PIXEL_PIN    6    // Digital IO pin connected to the NeoPixels.
+
+#define PIXEL_COUNT 3
+
+// Parameter 1 = number of pixels in strip,  neopixel stick has 8
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+int showType = 0;
+
+ClickEncoder *encoder;
+int16_t last, value, counter;
+
+void timerIsr() {
+  encoder->service();
+}
+
+void setup() {
+  Serial.begin(9600);
+  encoder = new ClickEncoder(A1, A0, A2);
+
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(timerIsr); 
+  
+  last = -1;
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+
+void loop() {  
+  bool updated = false;
+  int16_t movement = encoder->getValue();
+  value += movement;
+  
+  if (movement != 0 && value % 4 == 0) {
+    showType += value/4 - counter;
+    counter = value/4;
+    updated = true;
+  }
+  
+  if (value != last) {
+    last = value;
+    Serial.print("Encoder Value: ");
+    Serial.println(value);
+  }
+  
+  ClickEncoder::Button b = encoder->getButton();
+  if (b != ClickEncoder::Open) {
+    Serial.print("Button: ");
+    #define VERBOSECASE(label) case label: Serial.println(#label); break;
+    switch (b) {
+      VERBOSECASE(ClickEncoder::Pressed);
+      VERBOSECASE(ClickEncoder::Held)
+      VERBOSECASE(ClickEncoder::Released)
+      case ClickEncoder::Clicked:
+          Serial.println("ClickEncoder::Clicked");
+          showType++;
+          updated = true;
+          break;
+      case ClickEncoder::DoubleClicked:
+          Serial.println("ClickEncoder::DoubleClicked");
+          encoder->setAccelerationEnabled(!encoder->getAccelerationEnabled());
+          Serial.print("  Acceleration is ");
+          Serial.println((encoder->getAccelerationEnabled()) ? "enabled" : "disabled");
+          break;
+    }
+  }
+
+  if (updated) {
+    if (showType > 8)
+      showType=0;
+    if (showType < 0)
+      showType=9;
+    Serial.print("Updated! New type: ");
+    Serial.println(showType);
+    
+    startShow(showType);
+  }
+}
+
+
+void startShow(int i) {
+  switch(i){
+    case 0: colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
+            break;
+    case 1: colorWipe(strip.Color(255, 0, 0), 50);  // Red
+            break;
+    case 2: colorWipe(strip.Color(0, 255, 0), 50);  // Green
+            break;
+    case 3: colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+            break;
+    case 4: theaterChase(strip.Color(127, 127, 127), 50); // White
+            break;
+    case 5: theaterChase(strip.Color(127,   0,   0), 50); // Red
+            break;
+    case 6: theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+            break;
+    case 7: rainbow(20);
+            break;
+    case 8: rainbowCycle(20);
+            break;
+    case 9: theaterChaseRainbow(50);
+            break;
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j+=4) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
